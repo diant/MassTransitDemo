82c7ef6fc2c3d76ec1f75d3b82137e7ca08c17b9
diamantis <diant@live.com>
2023-10-08 20:35:01 +0200

    Swagger support

--- MassTransitDemo/Program.cs
+++ MassTransitDemo/Program.cs
@@ -1,11 +1,22 @@
 using MassTransitDemo;
-using MassTransitDemo.WeatherForecast; 
+using MassTransitDemo.WeatherForecast;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args)
     .AddMassTransitWithMediator()
-    .BindConfig(); 
+    .BindConfig();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.MapWeatherForecastEndpoints();
--- MassTransitDemo/WeatherForecast/MapEndpoints.cs
+++ MassTransitDemo/WeatherForecast/MapEndpoints.cs
@@ -5,9 +5,9 @@ namespace MassTransitDemo.WeatherForecast;
 
  internal static class MapEndpoints
 {
+    private const string baseRoute = "/api";
     internal static IApplicationBuilder MapWeatherForecastEndpoints(this WebApplication app)
     {
-        const string baseRoute = "/api";
         const string controllerRoute = "weatherforecast";       
 
         app.MapGet($"{baseRoute}/{controllerRoute}", async (IMediator mediator) =>
